e56420b2632888034fb664c2074a9020eef5b428
LiuLiang <liang.liu@castbox.fm>
2020-02-21 16:28:07 +0800

    Extract method

--- library/src/main/java/org/antennapod/audio/SonicAudioPlayer.java
+++ library/src/main/java/org/antennapod/audio/SonicAudioPlayer.java
@@ -732,16 +732,7 @@ public class SonicAudioPlayer extends AbstractAudioPlayer {
                                     int maxBytes = (available / 4) * 4;
                                     mSonic.readBytesFromStream(modifiedSamples, maxBytes);
 
-                                    for (int i = 0; (i + 3) < modifiedSamples.length; i += 4) {
-                                        short left = (short) ((modifiedSamples[i] & 0xff) | (modifiedSamples[i + 1] << 8));
-                                        short right = (short) ((modifiedSamples[i + 2] & 0xff) | (modifiedSamples[i + 3] << 8));
-                                        short value = (short) (0.5 * left + 0.5 * right);
-
-                                        modifiedSamples[i] = (byte) (value & 0xff);
-                                        modifiedSamples[i + 1] = (byte) (value >> 8);
-                                        modifiedSamples[i + 2] = (byte) (value & 0xff);
-                                        modifiedSamples[i + 3] = (byte) (value >> 8);
-                                    }
+                                    downMix(modifiedSamples);
                                     mTrack.write(modifiedSamples, 0, maxBytes);
                                 } else {
                                     mSonic.readBytesFromStream(modifiedSamples, available);
@@ -866,6 +857,19 @@ public class SonicAudioPlayer extends AbstractAudioPlayer {
                 return "STATE_ERROR";
             default:
                 return "UNKNOWN_STATE";
+        }
+    }
+
+    private void downMix(byte[] modifiedSamples) {
+        for (int i = 0; (i + 3) < modifiedSamples.length; i += 4) {
+            short left = (short) ((modifiedSamples[i] & 0xff) | (modifiedSamples[i + 1] << 8));
+            short right = (short) ((modifiedSamples[i + 2] & 0xff) | (modifiedSamples[i + 3] << 8));
+            short value = (short) (0.5 * left + 0.5 * right);
+
+            modifiedSamples[i] = (byte) (value & 0xff);
+            modifiedSamples[i + 1] = (byte) (value >> 8);
+            modifiedSamples[i + 2] = (byte) (value & 0xff);
+            modifiedSamples[i + 3] = (byte) (value >> 8);
         }
     }
 
